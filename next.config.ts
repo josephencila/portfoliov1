eb94899d7b3bac321a665cfc1d503d444aca626a
josephencila <joseph.mesia.encila@gmail.com>
2025-10-21 21:51:53 +0800

    refactor: next config, added svgr and image protocols

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co' },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    })
+
+    return config
+  },
 };
 
 export default nextConfig;
