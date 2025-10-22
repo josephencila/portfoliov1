303a29e10eaa2b873408f3fa9a432970bdfd88b1
josephencila <joseph.mesia.encila@gmail.com>
2025-10-22 22:47:20 +0800

    refactor: changes navigations type; added site header navigation, theme, logo, nav links, buttons; added site header to layout

--- app/(site)/_components/site-header.tsx
+++ app/(site)/_components/site-header.tsx
@@ -1,6 +1,61 @@
+"use client"
+
+import Link from "next/link"
+import { Icon } from "@iconify/react"
+import { MainLogo } from "@/public/assets/svgs"
+
+import { useTheme } from "next-themes"
+import { useToggleStore } from "@/store/useToggleStore"
+
+import { siteNavigations } from "@/lib/constants/navigations"
+
+import ListComponent from "@/components/shared/list.component"
 
 export default function SiteHeader() {
+    const { theme, setTheme } = useTheme()
+    const { toggleOn } = useToggleStore()
+
+    const handleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
-        <div>SiteHeader</div>
+        <header>
+            <nav className="h-full">
+                <ul className="flex flex-row items-center justify-between px-6 h-full gap-4">
+                    <li className="flex flex-row justify-center items-center w-full ">
+                        <Link href="/" className="flex flex-row gap-2 items-center w-full group">
+                            <MainLogo className=" w-10 h-10 pt-2 fill-neutral-900 hover:fill-neutral-800 dark:fill-neutral-50 dark:hover:fill-neutral-100 transition-colors" />
+                            <span className="uppercase text-xl font-semibold hidden md:block text-neutral-900 hover:text-neutral-800 dark:text-neutral-50 dark:hover:text-neutral-100 transition-colors">
+                                Joseph Encila
+                            </span>
+                        </Link>
+                    </li>
+                    <li className="flex flex-row gap-6">
+                        <ListComponent
+                            className="flex-row gap-4 hidden md:flex"
+                            data={siteNavigations}
+                            renderItem={((navigations) => (
+                                <Link
+                                    key={navigations.id}
+                                    href={navigations.href}
+                                    className="uppercase font-semibold transition-colors duration-300 delay-75 text-neutral-900 dark:text-neutral-100 hover:text-indigo-400 dark:hover:text-indigo-400"
+                                >
+                                    {navigations.name}
+                                </Link>
+                            ))}
+                        />
+                        <div className="flex flex-row gap-4">
+                            <button onClick={handleTheme} className="cursor-pointer">
+                                <Icon icon="material-symbols:ev-shadow-outline" className="w-6 h-6 text-neutral-900 dark:text-neutral-100   " />
+                            </button>
+                            <button onClick={toggleOn} className="cursor-pointer md:hidden">
+                                <Icon icon="icomoon-free:menu" className="w-6 h-6 text-neutral-900 dark:text-neutral-100   " />
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,17 @@
 
-const SiteLayout = () => {
+import SiteHeader from "@/app/(site)/_components/site-header"
+
+const SiteLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>SiteLayout</div>
+        <div className='min-h-full h-auto w-full bg-c-light-bg-1 dark:bg-c-dark-bg-1 flex flex-col items-center  transition-all ease-in-out'>
+
+            <div className='min-h-dvh h-full w-full max-w-7xl bg-white dark:bg-black ease-in-out  grid grid-cols-1 grid-rows-[64px_1fr]'>
+                <SiteHeader />
+                <main className='flex flex-col h-full '>
+                    {children}
+                </main>
+            </div>
+        </div>
     )
 }
 
