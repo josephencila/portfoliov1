3ff3ea5a6476343e592ddf4e2af3b902156f7597
josephencila <joseph.mesia.encila@gmail.com>
2025-10-22 22:10:34 +0800

    feat: constants

---
+++ lib/constants/_types/navigations.ts
@@ -0,0 +1,5 @@
+export type TNavigations = {
+    id: number
+    name: string
+    href: string
+}
---
+++ lib/constants/navigations.d.ts
@@ -0,0 +1,19 @@
+import { TNavigations } from "@/lib/constants/_types/navigations";
+
+const siteNavigations: TNavigations[] = [
+    {
+        id: 1,
+        name: "Home",
+        href: "/"
+    },
+    {
+        id: 2,
+        name: "About",
+        href: "/about"
+    },
+    {
+        id: 3,
+        name: "Contact",
+        href: "/contact"
+    },
+]
