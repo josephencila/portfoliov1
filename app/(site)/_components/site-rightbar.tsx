cb769ac42945ccd22246165fdca77938d01d3ccc
josephencila <joseph.mesia.encila@gmail.com>
2025-10-23 12:16:35 +0800

    refactor: added site rightbar to site layout; added logo, menu toggle, theme toggle, navigation linnks to site rightbar

--- app/(site)/_components/site-rightbar.tsx
+++ app/(site)/_components/site-rightbar.tsx
@@ -1,6 +1,59 @@
+"use client"
+
+import Link from "next/link"
+import { Icon } from "@iconify/react"
+import { MainLogo } from "@/public/assets/svgs"
+
+import { useTheme } from "next-themes"
+import { useToggleStore } from "@/store/useToggleStore"
+
+import { siteNavigations } from "@/lib/constants/navigations"
+
+import ListComponent from "@/components/shared/list.component"
+
 
 export default function SiteRightbar() {
+
+    const { theme, setTheme } = useTheme()
+    const { toggleOff } = useToggleStore()
+
+    const handleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
     return (
-        <div>SiteRightbar</div>
+        <aside className="bg-white dark:bg-black px-6 left-0 right-0 bottom-0 top-0 fixed grid grid-rows-[64px_1fr]">
+            <div className="flex flex-row justify-center items-center w-full border-b border-neutral-500 ">
+                <Link href="/" className="flex flex-row gap-2 items-center w-full group">
+                    <MainLogo className=" w-10 h-10 pt-2 fill-neutral-900 hover:fill-neutral-800 dark:fill-neutral-50 dark:hover:fill-neutral-100 transition-colors" />
+                    <span className="uppercase text-xl font-semibold hidden md:block text-neutral-900 hover:text-neutral-800 dark:text-neutral-50 dark:hover:text-neutral-100 transition-colors">
+                        Joseph Encila
+                    </span>
+                </Link>
+
+                <div className="flex flex-row gap-4">
+                    <button onClick={handleTheme} className="cursor-pointer">
+                        <Icon icon="material-symbols:ev-shadow-outline" className="w-6 h-6 text-neutral-900 dark:text-neutral-100   " />
+                    </button>
+                    <button onClick={toggleOff} className="cursor-pointer md:hidden">
+                        <Icon icon="rivet-icons:close" className="w-6 h-6 text-neutral-900 dark:text-neutral-100   " />
+                    </button>
+                </div>
+            </div >
+            <ListComponent
+                className="flex flex-col gap-4  py-8"
+                data={siteNavigations}
+                renderItem={((navigations) => (
+                    <Link
+                        onClick={toggleOff}
+                        key={navigations.id}
+                        href={navigations.href}
+                        className="uppercase font-semibold transition-colors duration-300 delay-75 text-neutral-900 dark:text-neutral-100 hover:text-indigo-400 dark:hover:text-indigo-400"
+                    >
+                        {navigations.name}
+                    </Link>
+                ))}
+            />
+        </aside>
     )
 }
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,12 +1,19 @@
+"use client"
+
+import { useToggleStore } from "@/store/useToggleStore"
 
 import SiteHeader from "@/app/(site)/_components/site-header"
+import SiteRightbar from "@/app/(site)/_components/site-rightbar"
 
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
+
+    const { isToggled } = useToggleStore()
+
     return (
         <div className='min-h-full h-auto w-full bg-c-light-bg-1 dark:bg-c-dark-bg-1 flex flex-col items-center  transition-all ease-in-out'>
-
             <div className='min-h-dvh h-full w-full max-w-7xl bg-white dark:bg-black ease-in-out  grid grid-cols-1 grid-rows-[64px_1fr]'>
                 <SiteHeader />
+                {isToggled && <SiteRightbar />}
                 <main className='flex flex-col h-full '>
                     {children}
                 </main>
