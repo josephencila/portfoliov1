bae9e45f47ec7e0feab29d2cda6dc00299dd2873
josephencila <joseph.mesia.encila@gmail.com>
2025-10-23 19:42:13 +0800

    refactor: site footer, added copyright date and of social media list

---
+++ app/(site)/_components/site-footer.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link"
+import { Icon } from "@iconify/react"
+
+import ListComponent from "@/components/shared/list.component"
+
+import { socialList } from "@/lib/constants/mock-data"
+
+
+export default function SiteFooter() {
+
+    const date = new Date()
+    const year = date.getFullYear()
+
+    return (
+        <footer className="bg-white dark:bg-black px-6">
+            <div className="grid grid-cols-2  border-t border-neutral-300 h-full">
+                <div className="flex items-center ">
+                    <p className="uppercase text-sm md:text-base ">@ {year} {process.env.NEXT_PUBLIC_ABBR_NAME}</p>
+                </div>
+                <ListComponent
+                    as="ul"
+                    className="flex flex-row gap-2 items-center justify-end"
+                    data={socialList}
+                    renderItem={(social) => (
+                        <li key={social.id}>
+                            <Link href={social.url}>
+                                <Icon icon={social.icon} className="w-6 h-6 text-neutral-900 dark:text-neutral-100   " />
+                            </Link>
+                        </li>
+                    )}
+                />
+            </div>
+        </footer>
+    )
+}
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -6,6 +6,7 @@ import useScreenSize from "@/lib/hooks/use-screen-size"
 
 import SiteHeader from "@/app/(site)/_components/site-header"
 import SiteRightbar from "@/app/(site)/_components/site-rightbar"
+import SiteFooter from "@/app/(site)/_components/site-footer"
 
 const SiteLayout = ({ children }: { children: React.ReactNode }) => {
 
@@ -20,12 +21,13 @@ const SiteLayout = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <div className='min-h-full h-auto w-full bg-c-light-bg-1 dark:bg-c-dark-bg-1 flex flex-col items-center  transition-all ease-in-out'>
-            <div className='min-h-dvh h-full w-full max-w-7xl bg-white dark:bg-black ease-in-out  grid grid-cols-1 grid-rows-[64px_1fr]'>
+            <div className='min-h-dvh h-full w-full max-w-7xl bg-white dark:bg-black ease-in-out  grid grid-cols-1 grid-rows-[64px_1fr_64px]'>
                 <SiteHeader />
                 {isToggled && <SiteRightbar />}
                 <main className='flex flex-col h-full '>
                     {children}
                 </main>
+                <SiteFooter />
             </div>
         </div>
     )
