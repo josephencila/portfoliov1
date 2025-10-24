3826fab7573c3b0a468317852a861387eeef43a0
josephencila <joseph.mesia.encila@gmail.com>
2025-10-24 13:59:07 +0800

    refactor: added stack list on projects page

--- app/(site)/projects/[slug]/page.tsx
+++ app/(site)/projects/[slug]/page.tsx
@@ -1,16 +1,38 @@
-import { IProjectPage } from "@/app/(site)/projects/_types";
 import Image from "next/image";
+
+import { IProjectPage } from "@/app/(site)/projects/_types";
+import { projectList } from "@/lib/constants/mock-data";
+
+import ListComponent from "@/components/shared/list.component";
 
 const SiteProjectsPage = async ({ params }: IProjectPage) => {
     const { slug } = await params;
     return (
-        <div className="flex flex-col px-6 py-8 gap-6">
-            <div className="grid grid-rows-2 md:grid-cols-2 md:gap-6">
+        <div className="flex flex-col px-6 py-8 gap-6 ">
+            <div className="grid grid-rows-2 md:grid-cols-2 gap-6">
                 <div className="flex flex-col gap-4">
                     <h1 className="uppercase text-3xl md:text-4xl font-bold">
                         Adtocarto
                     </h1>
                     <hr className="bg-neutral-300" />
+                    <span className="uppercase">Stack:</span>
+                    <ListComponent
+                        data={projectList.slice(0, 1)}
+                        as="ul"
+                        renderItem={(project) => (
+                            <ListComponent
+                                key={project.id}
+                                data={project.technologies}
+                                as="li"
+                                className="flex flex-row gap-2 flex-wrap uppercase"
+                                renderItem={(tech) => (
+                                    <div>
+                                        {tech.name}
+                                    </div>
+                                )}
+                            />
+                        )}
+                    />
                 </div>
                 <div>
                     <p className="text-base font-medium md:text-lg uppercase leading-relaxed text-neutral-600 dark:text-neutral-300 max-w-2xl ">
